b753c8a8be70bb089e2658598b6450536b1ca172
bhushan <97792475+Godshivu-172007@users.noreply.github.com>
2025-02-28 17:13:49 +0530

    Update vite.config.js

--- forentend/vite.config.js
+++ forentend/vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api': 'http://localhost:3000'
-    },
+export default defineConfig(({ mode }) => ({
+  server: {
+    proxy: mode === 'development' ? {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    } : {
+      '/api': {
+        target: 'https://full-stack-mo36.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
   },
   plugins: [react()],
-})
+}));
